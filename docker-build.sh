ec38bf0ecafeab04652fbf061f36b0897961b998
David Lippi <david.lippi@hibo.it>
2023-01-04 16:42:40 +0100

    downgraded from java 17 to java 11, set utf-8 encoding

--- docker-build.sh
+++ docker-build.sh
@@ -2,6 +2,7 @@
 
 set -ex
 
-docker run -it --rm --name keycloak-anotherhashes -v "$(pwd)":/usr/src/mymaven -w /usr/src/mymaven maven:3.3-jdk-8 mvn clean package
-echo "cp target/keycloak-anotherhashes-1.2.jar [your]/keycloak/deployments/"
-#echo "cp target/keycloak-anotherhashes-1.2.jar ../../../dxp-services/keycloak/deployments/"
+docker run -it --rm --name keycloak-anotherhashes -v "$(pwd)":/usr/src/mymaven -w /usr/src/mymaven maven:3.8.7 mvn clean package
+
+echo "cp target/keycloak-anotherhashes-20.0.2.jar [your]/keycloak/deployments/"
+#echo "cp target/keycloak-anotherhashes-20.0.2.jar ../../../dxp-services/keycloak/deployments/"
