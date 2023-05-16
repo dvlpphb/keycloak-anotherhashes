069144d22760280d96fa5022ee690889391bb0fb
David Lippi <david.lippi@hibo.it>
2023-05-16 15:04:25 +0200

    Upgraded to keycloak 21.1.1

--- docker-build.sh
+++ docker-build.sh
@@ -2,7 +2,9 @@
 
 set -ex
 
+KEYCLOAK_VERSION="21.1.1"
+
 docker run -it --rm --name keycloak-anotherhashes -v "$(pwd)":/usr/src/mymaven -w /usr/src/mymaven maven:3.8.7 mvn clean package
 
-echo "cp target/keycloak-anotherhashes-20.0.2.jar [your]/keycloak/deployments/"
-#echo "cp target/keycloak-anotherhashes-20.0.2.jar ../../../dxp-services/keycloak/deployments/"
+echo "cp target/keycloak-anotherhashes-${KEYCLOAK_VERSION}.jar [your]/keycloak/deployments/"
+#echo "cp target/keycloak-anotherhashes-${KEYCLOAK_VERSION}.jar ../../../dxp-services/keycloak/deployments/"
